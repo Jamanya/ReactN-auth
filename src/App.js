f93075aab69463602df4bee8b4d9b128c0e19cfc
Jamanya <richardson.james@outlook.com>
2017-08-20 21:51:52 -0400

    added auth for app init in firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
+
+
 class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyAr3qUeWJovYk9hQ3ZJfvnCOzIil1Qh02I',
+            authDomain: 'reactn-auth.firebaseapp.com',
+            databaseURL: 'https://reactn-auth.firebaseio.com',
+            projectId: 'reactn-auth',
+            storageBucket: 'reactn-auth.appspot.com',
+            messagingSenderId: '955502263583'
+          });
+    }
+
     render() {
         return (
             <View>
